a0760916b3e357c696687f51b8d786a1002f5aed
vallenizaac <isengiseng339@gmail.com>
2020-11-09 15:44:08 +0800

    Ninth Commit

---
+++ pertemuan 23/index.js
@@ -0,0 +1,58 @@
+//variabel
+
+//let & cons
+
+// let _umur = 25;
+// _umur = 26;
+// // let nilai = 95.5;
+// // let sudahMenikah = false;
+// // let $nama = "John Doe";
+// // console.log(_umur);
+// // console.log($nama);
+
+//     let alamat;
+//     console.log(alamat);
+
+// // const tahunLahir = 2010;
+// // console.log(tahunLahir);
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log("Nama Saya" + namaDepan + " " namaBelakang " " + umur " ");
+
+//ternary operator
+// let angka1 = 10;
+// let angka2 = 9;
+
+// console.log(angka1 <= angka2);
+
+// //operator logika
+// let kondisi1 = 1<2;
+// let kondisi2 = true;
+
+// console.log(kondisi1 || kondisi2);
+// console.log(!log)
+
+let berat = prompt("Masukkan Berat: ");
+console.log("Berat yang dimasukkan adalah " + berat);
+let tinggi = prompt("Masukkan Tinggi: ");
+console.log("Tinggi yang dimasukkan adalah " + tinggi);
+
+//
+/*
+    Buatlah program javascript untuk menghitung BMI (Body Mass Index)
+    Rumus = berat {kg} / (tinggi * tingi);
+*/    
+
+let bilangan1=berat;
+let bilangan2=tinggi;
+
+console.log(bilangan1 / bilangan2 * bilangan2);
+
+
+
