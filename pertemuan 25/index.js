2356ad4928fb77601bfd195b658ab2791d2a21c2
vallenizaac <isengiseng339@gmail.com>
2020-11-23 15:43:35 +0800

    Sixthtenn Commit

--- pertemuan 25/index.js
+++ pertemuan 25/index.js
@@ -1,74 +1,38 @@
-//Array
+// // console.log("JavaSript DOM")
 
-let nilai = [90, 80, 75, 100, 85];
+// // const person = ("john", 24, "programmer", true);
+// const person = {
+//     name: "john",
+//     age: 24,
+//     job: "programmer",
+//     isMarried: true,
+//     sayHello: function()
+//     {
+//         console.log("Hello World");
+//     }
+// };
 
-for (let i = 0; i< nilai.length; i++)
+// person.age = 25;
+// person["address"] = "Manado";
+
+// console.log(person.name);
+// console.log(person["age"]);
+
+// delete person.address;
+// console.log(person);
+
+// person.sayHello();
+
+let title = document.getElementsByTagName("h1")[0].innerText;
+
+title = document.getElementById["header2"].innerText;
+console.log(title);
+
+let umur = document.getElementById("umur").value;
+console.log(umur);
+
+function helloWorld()
 {
-    console.log("Nilai = ",nilai[i] +5);
+    let value = document.getElementById("umur").value;
+    console.log(value);
 }
-console.log(nilai);
-
-// nilai[2] = 90;
-// console.log(nilai[nilai.length -1]);
-
-let john = ["John", "Doe", 33, true];
-console.log(john[john.length -1]);
-console.log(john);
-
-// toString()
-console.log(john.toString());
-// join
-console.log(john.join(" - "));
-
-// pop()
-john.pop();
-console.log(john.join(" - "));
-// push()
-john.push(true);
-john.push("Hello");
-console.log(john.join(" - "));
-
-// shift()
-john.shift();
-console.log(john.join(" - "));
-
-// unshift()    
-john.unshift("John"); 
-john.unshift("Prof");
-console.log(john.join(" - "));
-
-// splice()
-let buah = ["Apel", "Pepaya", "Durian", "Jeruk"];
-
-buah.splice(2, 0, "Lemon", "Manggis");
-console.log(buah);
-
-// concat()
-let honda = ["Sonic 150R", "CBR 150R", "Vario"];
-let yamaha = ["Mio", "R15", "MX King"];
-let suzuki = ["Satria 150", "Nex", "Smash"];
-
-let motor = honda.concat(yamaha, suzuki);
-console.log(motor);
-
-// slice()
-let cemilan = ["kripik singkong", "kripik kentang", "krupuk ikan", "permen", "coklat", "kue"];
-let cemilanGurih = cemilan.slice(0, 3);
-let cemilanManis = cemilan.slice(3);
-
-console.log(cemilanGurih);
-console.log(cemilanManis);
-
-// sort()
-let bilangan = [30, 1, 2, 3.5, 10, 100];
-
-bilangan.sort();
-
-console.log(bilangan);
-
-// reverse()
-let buah = ["Jeruk", "Apel", "Mangga", "Jambu"];
-
-buah.reverse();
-
-console.log(buah);
